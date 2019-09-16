0e4d93ee95e52871f4cf8c7f32bb2b37bb55191e
pearcej <jan_pearce@berea.edu>
2019-09-16 08:33:52 -0400

    small changes

--- bigo-explorer/bigo-explorer-main.cpp
+++ bigo-explorer/bigo-explorer-main.cpp
@@ -1,4 +1,4 @@
-/*This program is designed to explore big o notion and algorithms 
+/*This program is designed to explore big O notion and algorithms 
 by Dr. Jan Pearce of Berea College
 Licensed under a Creative Commons Attribution,
 Share Alike 3.0 United States License */
@@ -8,96 +8,108 @@ Share Alike 3.0 United States License */
 
 using namespace std;
 
-int looponconstant(int n) {
-	/* looponconstant demonstrates O(1) behavior
-	pre: n is a positive integer
-	pos:{ returns an integer count of the number of iterations*/
+int looponconstant1(int n) {
+	/*  demonstrates O(1) behavior
+	pre: n is positive integer
+	post: returns count # iterations*/
 
 	int count = 0;
 	for (int i = 0; i < 1; i++) {
-		count = count + 1;   ////we are interested in how often this line executes
+		count++; // # this line executes
 	return (count);
 	}
 }
 
-int looponconstant2(int n) {
-	/* looponconstant2 also demonstrates O(1) behavior
+int looponconstant4(int n) {
+	/* demonstrates O(1) behavior
 	pre: n is a positive integer
-	post: returns an integer count of the number of iterations */
+	post: returns count # iterations*/
 
 	int count = 0;
-	for (int i = 0; i < 10; i++) {
-		count = count + 1;   //we are interested in how often this line executes
+	for (int i = 0; i < 4; i++) {
+		count++; // # this line executes
 	}
 	return (count);
 }
 
-int loopwn(int n) {
-	/* loopwn demonstrates O(n) behavior
+int loopn(int n) {
+	/* demonstrates O(n) behavior
 	pre:  n is a positive integer
-	post:  returns an integer count of the number of iterations */
+	post:  returns count # iterations */
 
 	int count = 0;
 	for (int i = 0; i < n; i++) {
-		count = count + 1; //we are interested in how often this line executes
+		count++; // # this line executes
+		}
+	return (count);
+	}
+
+int loopincrementn(int n) {
+	/* demonstrates O(n) behavior
+	pre:  n is a positive integer
+	post: returns count # iterations */
+
+	int count = 0;
+	int s = 10;
+	int add = 2;
+	for (int i = s; i < n; i=i+add) {
+		count++; // # this line executes
 	}
 	return (count);
 }
-
 
 int sequential(int n) {
-	/* sequential also demonstrates O(n) behavior
-	pre: n is a positive integer
-	post: returns an integer count of the number of iterations */
-
+	/* demonstrates O(n) behavior
+	pre: n is positive integer
+	post: returns count # iterations */
 	int count = 0;
 	for (int i = 0; i < n; i++) {
-		count = count + 1;   //we are interested in how often this line executes   
+		count++; //this # iterations
 	}
 	for (int i = 0; i < n; i++) {
-		count = count + 1;   //we are interested in how often this line executes
+		count++;	//plus this # iterations
 	}
 	return (count);
 }
 
-int nestedwconstant(int n) {
-	/* nestedwconstant demonstrates O(n) behavior
+int nestedconstant(int n) {
+	/* demonstrates O(n) behavior
 	pre:  n is a positive integer
-	post:  returns an integer count of the number of iterations */
+	post: returns count # iterations*/
 
 	int count = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < 10; j++) {
-			count = count + 1;   //we are interested in how often this line executes
+			count++;  // # this line executes
 		}
 	}
 	return (count);
 }
 
 int twonested(int n) {
-	/* twonested demonstrates O(n^^2) behavior
-	pre:  n is a positive integer
-	post:  returns an integer count of the number of iterations */
+	/* demonstrates O(n^^2) behavior
+	pre:  n is positive integer
+	pot: returns count # iterations*/
 
 	int count = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			count = count + 1;   //we are interested in how often this line executes
+			count++;  // # this line executes		
 		}
 	}
 	return (count);
 }
 
 int threenested(int n) {
-	/* loopwn demonstrates O(n**3) behavior
+	/* threenested demonstrates O(n**3) behavior
 	pre:  n is a positive integer
-	post:  returns an integer count of the number of iterations */
+	post: returns count # iterations*/
 
 	int count = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			for (int k = 0; k < n; k++) {
-				count = count + 1;   //we are interested in how often this line executes
+				count++;  // # this line executes				
 			}
 		}
 	}
@@ -106,26 +118,26 @@ int threenested(int n) {
 
 int powern(int n) {
 	/* powern demonstrates O(2**n)) behavior
-	pre:  n is a positive integer
-	post:  returns an integer count of the number of iterations */
+	pre: n is a positive integer
+	pot: returns count # iterations*/
 
 	int count = 0;
 	for (int i = 0; i < pow(2,n); i++) {
-		count = count + 1;   //we are interested in how often this line executes
+		count++; // # this line executes
 	}
 	return (count);
 }
 
 int halving(int n) {
-	/* twonested demonstrates O(log(int n)) behavior
+	/*  demonstrates O(log(int n)) behavior
 	pre:  n is a positive integer
-	post:  returns an integer count of the number of iterations */
+	pot: returns count # iterations*/
 
 	int count = 0;
 	int loopcounter = n;
 	while (loopcounter > 1) {
-		count = count + 1;   //we are interested in how often this line executes
-		loopcounter = loopcounter / 2; // in C++ this is integer division
+		count++;  // # this line executes
+		loopcounter = loopcounter / 2; // integer division
 	}
 	return (count);
 }
@@ -136,12 +148,14 @@ int main() {
 	cin >> n;
 
 	while (n > 0) {
+		// execution based on # inputs
 		cout << "\nfor n = " << n << ":" << endl;
-		cout << "\tlooponconstant(" << n << ") = " << looponconstant(n) << endl;
-		cout << "\tlooponconstant2(" << n << ") = " << looponconstant2(n) << endl;
-		cout << "\tloopwn(" << n << ") = " << loopwn(n) << endl;
+		cout << "\tlooponconstant1(" << n << ") = " << looponconstant1(n) << endl;
+		cout << "\tlooponconstant4(" << n << ") = " << looponconstant4(n) << endl;
+		cout << "\tloopn(" << n << ") = " << loopn(n) << endl;
+		cout << "\tloopincrementn(" << n << ") = " << loopincrementn(n) << endl;
 		cout << "\tsequential(" << n << ") = " << sequential(n) << endl;
-		cout << "\tnestedwconstant(" << n << ") = " << nestedwconstant(n) << endl;
+		cout << "\tnestedconstant(" << n << ") = " << nestedconstant(n) << endl;
 		cout << "\ttwonested(" << n << ") = " << twonested(n) << endl;
 		cout << "\tthreenested(" << n << ") = " << threenested(n) << endl;
 		cout << "\tpowern(" << n << ") = " << powern(n) << endl;
